5eec5c5594604cb11f537d760ed49e9b5c632cab
FlameKat53 <flame9804@yahoo.com>
2020-02-28 09:27:24 -0600

    Add files via upload

---
+++ mkd.bat
@@ -0,0 +1,79 @@
+mkdir #
+mkdir A
+mkdir B
+mkdir C
+mkdir D
+mkdir E
+mkdir F
+mkdir G
+mkdir H
+mkdir I
+mkdir J
+mkdir K
+mkdir L
+mkdir M
+mkdir N
+mkdir O
+mkdir P
+mkdir Q
+mkdir R
+mkdir S
+mkdir T
+mkdir U
+mkdir V
+mkdir W
+mkdir X
+mkdir Y
+mkdir Z
+move A*.* A/
+move B*.* B/
+move C*.* C/
+move D*.* D/
+move E*.* E/
+move F*.* F/
+move G*.* G/
+move H*.* H/
+move I*.* I/
+move J*.* J/
+move K*.* K/
+move L*.* L/
+move N*.* N/
+move O*.* O/
+move P*.* P/
+move Q*.* Q/
+move R*.* R/
+move S*.* S/
+move T*.* T/
+move U*.* U/
+move V*.* V/
+move W*.* W/
+move X*.* X/
+move Y*.* Y/
+move Z*.* Z/
+rmdir #
+rmdir A
+rmdir B
+rmdir C
+rmdir D
+rmdir E
+rmdir F
+rmdir G
+rmdir H
+rmdir I
+rmdir J
+rmdir K
+rmdir L
+rmdir M
+rmdir N
+rmdir O
+rmdir P
+rmdir Q
+rmdir R
+rmdir S
+rmdir T
+rmdir U
+rmdir V
+rmdir W
+rmdir X
+rmdir Y
+rmdir Z
